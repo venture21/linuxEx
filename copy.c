509edaa6669b5aeb7a53196ed8d4a9a8a3351b53
heejin park <park0122@gmail.com>
2018-05-18 10:39:25 +0900

    update copy.c
    gettimeofday함수 추가
    시간 측정 가능

--- copy.c
+++ copy.c
@@ -3,13 +3,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <sys/time.h>
 #define BUFSIZE 1024
 
 int main(int argc, char **argv)
 {
     int byteCount, fdIn, fdOut;
     char buf[BUFSIZE];
- 
+ 	struct timeval StartTime, EndTime, ProcTime;
+
     if(argc <3)
     {
         //write(2, "Usage : copy file1 file2\n", 25);
@@ -17,6 +19,7 @@ int main(int argc, char **argv)
         return -1;
     }
 
+	gettimeofday(&StartTime, NULL);
     // 원본파일 열기
     if((fdIn = open(argv[1], O_RDONLY))<0)
     {
@@ -37,5 +40,19 @@ int main(int argc, char **argv)
     close(fdIn);
     close(fdOut);
     
+	gettimeofday(&EndTime, NULL);
+
+	if((EndTime.tv_usec-StartTime.tv_usec)<0)
+	{
+		ProcTime.tv_sec  = EndTime.tv_sec - StartTime.tv_sec - 1;
+		ProcTime.tv_usec = 1000000 + EndTime.tv_usec - StartTime.tv_usec;
+	} 
+	else
+	{
+		ProcTime.tv_sec  = EndTime.tv_sec - StartTime.tv_sec;
+		ProcTime.tv_usec = EndTime.tv_usec - StartTime.tv_usec; 
+	}
+
+	printf("Copy Time : %ld.%ldusec\n",ProcTime.tv_sec, ProcTime.tv_usec);
     return 0;
 }
